e06494dfa3707fdacfdf592c327c5e1196fadca4
Faith Janicek <faithe1937@gmail.com>
2019-10-08 21:45:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,43 @@
 # My Code here....
+def map_to_negativize(array_collection)
+  final_array = []
+  counter = 0 
+  while counter < array_collection.length do 
+    final_array.push(array_collection[counter] * -1)
+    counter += 1 
+  end 
+final_array
+end 
+
+def map_to_no_change(dune_array)
+  final_array = []
+  counter = 0 
+  while counter < dune_array.length do 
+    final_array.push(source_array[counter])
+    counter += 1 
+  end 
+final_array
+end 
+
+def map_to_double(array_collection)
+  final_array = []
+  counter = 0 
+  while counter < array_collection.length do 
+    final_array.push(array_collection[counter] * 2)
+    counter += 1 
+  end 
+final_array
+end 
+
+def map_to_square(array_collection)
+  final_array = []
+  counter = 0 
+  while counter < array_collection.length do 
+    final_array.push(array_collection[counter] ** 2)
+    counter += 1 
+  end 
+final_array
+end 
+
+
+  
